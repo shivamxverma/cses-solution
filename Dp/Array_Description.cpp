b7bb678e5714bc6ca56d586f42a89d8115730a91
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-03-09 12:28:44 +0530

    Solved Array Description

--- Dp/Array_Description.cpp
+++ Dp/Array_Description.cpp
@@ -3,36 +3,8 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-int solve(int n,int xx,vector<int> &v,int m,vector<vector<int>> &dp){
-   if(n==0){
-    if(v[0]!=0){
-      if(abs(v[0]-xx)<=1)return 1;
-      return 0;
-    } else {
-      if(xx == 1){
-        if(m>xx)return 2;
-        else return 1;
-      }
-      else {
-        if(m>xx)return 3;
-        else return 2;
-      }
-    }
-   }
-   if(dp[n][xx]!=-1)return dp[n][xx];
-   int ans = 0;
-   if(v[n]==0){
-    for(int i=1 ; i<=m ; i++){
-       if(abs(xx-i)<=1 || xx == 100001){
-        ans += solve(n-1,i,v,m,dp);
-       }
-    }
-   } else{
-    ans += solve(n-1,v[n],v,m,dp);
-   }
-   return dp[n][xx] = ans;
-} 
+#define ll long long int
+const int mod = 1000000007;
 
 void MahavirCoder(){
   int n,m;cin>>n>>m;
@@ -40,9 +12,34 @@ void MahavirCoder(){
   for(int i=0 ; i<n ; i++){
     cin>>v[i];
   }
-  int xx = 100001;
-  vector<vector<int>> dp(n+1,vector<int>(xx+1,-1));
-  cout << solve(n-1,xx,v,m,dp) << endl;
+
+  vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+  
+  if(v[0]>0)dp[0][v[0]] = 1;
+  else for(int i=1 ; i<=m ; i++)dp[0][i] = 1;
+
+  for(int i=1 ; i<n ; i++){
+    if(v[i]==0){
+      for(int d = 1 ; d<=m ; d++){
+        if(d-1>=1)dp[i][d] = (dp[i][d] + dp[i-1][d-1])%mod;
+        dp[i][d] = (dp[i][d] + dp[i-1][d])%mod;
+        if(d+1<=m)dp[i][d] = (dp[i][d] + dp[i-1][d+1])%mod;
+      }
+    } else {
+      if(v[i]-1>=1)dp[i][v[i]] = (dp[i][v[i]] + dp[i-1][v[i]-1])%mod;
+      dp[i][v[i]] = (dp[i][v[i]] + dp[i-1][v[i]])%mod;
+      if(v[i]+1<=m)dp[i][v[i]] = (dp[i][v[i]] + dp[i-1][v[i]+1])%mod;
+    }
+  }
+
+  ll ans = 0;
+  for(int j = 1; j<=m; ++j){
+    (ans += dp[n - 1][j]) %= mod;
+  }
+ 
+  cout<<ans<<endl;
+
+  
 }
 
 int main(){
