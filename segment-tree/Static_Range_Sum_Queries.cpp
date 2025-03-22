dc0e6943be0e97b7d8aa44482085501257a631bd
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-03-22 09:31:44 +0530

    Solved Range Query PROBLEM Using Segment Tree

--- segment-tree/Static_Range_Sum_Queries.cpp
+++ segment-tree/Static_Range_Sum_Queries.cpp
@@ -2,32 +2,39 @@
 using namespace std;
 #define ll long long int
 
-const int N = 200000;
+const int N = 200007;
 
-void build(){
-   
+vector<ll> segTree(4*N+7,-1);
+
+void build(vector<ll> &a,int s,int e,int i){
+   if(s==e){
+    segTree[i] = a[s];
+    return ;
+   }
+   int mid = s + (e-s)/2;
+   build(a,s,mid,2*i);
+   build(a,mid+1,e,2*i+1);
+   segTree[i] = segTree[2*i] + segTree[2*i+1];
 }
 
-void update(){
-
-}
-
-void query(){
-
+ll query(int s,int e,int l,int r,int i){
+    if(s>r || e<l) return 0;
+    if(l <= s && e <= r) return segTree[i];
+    int m = s + (e-s)/2;
+    return query(s, m, l, r, 2*i) + query(m+1, e, l, r, 2*i+1);
 }
 
 void solve(){
     int n,q;cin>>n>>q;
-    vector<ll> pre = {0};
+    vector<ll> a(n);
     for(int i=0 ; i<n ; i++){
-      ll x;cin>>x;
-      pre.push_back(pre.back()+x);
+      cin>>a[i];
     }
-
+    build(a,0,n-1,1);
     while(q--){
       int l,r;cin>>l>>r;
-      l--;
-      cout << pre[r]-pre[l] << endl;
+      l--,r--;
+      cout << query(0,n-1,l,r,1) << endl;
     }
 }
 
@@ -35,3 +42,6 @@ int main(){
   solve();
   return 0;
 }
+
+
+
