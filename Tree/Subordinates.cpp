1f61cd14c05e787cab28c26a5a26f311fce624e7
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-04-10 20:23:29 +0530

    Solved Tree Problem

--- Tree/Subordinates.cpp
+++ Tree/Subordinates.cpp
@@ -1,91 +1,41 @@
 #include<bits/stdc++.h>
 #include<unordered_set>
 using namespace std;
- 
 #define int long long
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define vi vector<int>
-typedef pair<int,int> pi;
-#define setbits(x) __builtin_popcountll(x)
-#define binary_rep(x) bitset<sizeof(10)*8> binary_rep(x)
-const int M = 1000000007;
-#define endl "\n" 
-#define sum(a) (accumulate((a).begin(), (a).end(), 0LL))
-#define unique_el(s) sort(all(s)) ; s.erase(unique(s.begin(), s.end()), s.end())
-#define minel(a) (*min_element((a).begin(), (a).end()))
-#define maxel(a) (*max_element((a).begin(), (a).end()))
-#define mini(a) (min_element((a).begin(), (a).end()) - (a).begin())
-#define maxi(a) (max_element((a).begin(), (a).end()) - (a).begin())
-#define cnt(a, x) (count((a).begin(), (a).end(), (x)))
-#define lob(a, x) (lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
-#define upb(a, x) (upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
-#define pb push_back
-#define mp make_pair
-#define rep(i,a,b) for (int i = a; i < b; i++)
-#define Rep(i,b) for (int i = 0; i < b; i++)
-#define repr(i,a,b) for (int i = a; i >= b; i--)
-#define printyes cout << "YES" << "\n" 
-#define printno cout << "NO" << "\n" 
- 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-template <class T> void _print(T t){cerr<<t;}
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(unordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(unordered_multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
- 
-template <class T> istream & operator>> (istream &in, vector<T> &v) {
-    for (auto &vl : v) { in >> vl;} return in; }
-template <typename T> void pvec(vector<T>&v) {
-    for(auto i : v) {cout << i << " ";} cout << endl;}
-
-
-void dfs(int node,vector<vector<int>> &tree,vector<bool> &vis,vector<int> &ans){
-     vis[node] = true;
-     for(auto i : tree[node]){
-        if(!vis[i]){
-           ans[node]++;
-           dfs(i,tree,vis,ans);
-        }
-     }
-}
-void solve(){
-    int n;
-    cin>>n;
-    vi v(n+1);
-    v[1] = 0;
-    rep(i,2,n+1){
-        cin>>v[i];
+vector<vector<int>> tree;
+vector<int> ans;
+void dfs(int uu) {
+    ans[uu] = 1;
+    for(auto vv : tree[uu]) {
+        dfs(vv);
+        ans[uu] += ans[vv];
     }
-    vector<vector<int>> tree(n+1);
-    for(int i = 1; i <= n; i++){
+}
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n + 1);
+    v[1] = 0;
+    for(int i = 2; i <= n; i++) {
+        cin >> v[i];
+    } 
+    tree.resize(n + 1);
+    ans.resize(n + 1, 0);
+    for(int i = 1; i <= n; i++) {
         int u = i;
         int v_ = v[i];
-        tree[v_].pb(u);
+        tree[v_].push_back(u);
     }
-
-    vector<int> ans(n+1,0);
-    for(int i=1 ; i<=n ; i++){
-        vector<bool> vis(n,false);
-        dfs(i,tree,vis,ans);
-        debug(ans);
+    dfs(0); 
+    for(int i = 1; i <= n; i++) {  
+        cout << ans[i] << " ";  
     }
-    debug(tree);
-    // debug(ans);
+    cout << endl;
 }
-signed main(){
+signed main() {
     int t = 1;
-    // cin>>t;
-    while(t--){
+    while(t--) {
         solve();
     }
+    return 0;
 }
