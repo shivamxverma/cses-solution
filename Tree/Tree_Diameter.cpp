e59ee7d366d45bb47f1c3e93e2974f67c2a1ce99
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-04-10 20:23:52 +0530

    Solved Tree Problem

--- Tree/Tree_Diameter.cpp
+++ Tree/Tree_Diameter.cpp
@@ -1,108 +1,59 @@
-#include<bits/stdc++.h>
-#include<unordered_set>
+#include <bits/stdc++.h>
 using namespace std;
- 
-// #define int long long
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define vi vector<int>
-typedef pair<int,int> pi;
-#define setbits(x) __builtin_popcountll(x)
-#define binary_rep(x) bitset<sizeof(10)*8> binary_rep(x)
-const int M = 1000000007;
-#define endl "\n" 
-#define sum(a) (accumulate((a).begin(), (a).end(), 0LL))
-#define unique_el(s) sort(all(s)) ; s.erase(unique(s.begin(), s.end()), s.end())
-#define minel(a) (*min_element((a).begin(), (a).end()))
-#define maxel(a) (*max_element((a).begin(), (a).end()))
-#define mini(a) (min_element((a).begin(), (a).end()) - (a).begin())
-#define maxi(a) (max_element((a).begin(), (a).end()) - (a).begin())
-#define cnt(a, x) (count((a).begin(), (a).end(), (x)))
-#define lob(a, x) (lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
-#define upb(a, x) (upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
-#define pb push_back
-#define mp make_pair
-#define rep(i,a,b) for (int i = a; i < b; i++)
-#define Rep(i,b) for (int i = 0; i < b; i++)
-#define repr(i,a,b) for (int i = a; i >= b; i--)
-#define printyes cout << "YES" << "\n" 
-#define printno cout << "NO" << "\n" 
- 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-template <class T> void _print(T t){cerr<<t;}
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(unordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(unordered_multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
- 
-template <class T> istream & operator>> (istream &in, vector<T> &v) {
-    for (auto &vl : v) { in >> vl;} return in; }
-template <typename T> void pvec(vector<T>&v) {
-    for(auto i : v) {cout << i << " ";} cout << endl;}
 
-int realnode = 0;
-int max_depth = -1;
-int max_depth1 = -1;
-int dfs(vector<vector<int>> &tree, int node, vector<bool> &vis, int depth) {
-    vis[node] = true;
-    if (depth > max_depth) {
-        max_depth = depth;
-        realnode = node;
-    }
-    for (auto i : tree[node]) {
-        if (!vis[i]) {
-            dfs(tree, i, vis, depth + 1);
+vector<vector<int>> tree;
+int diameter = 0;
+
+int dfs(int node, int parent)
+{
+    int max1 = -1, max2 = -1;
+    for (int child : tree[node])
+    {
+        if (child == parent)
+            continue;
+        int h = dfs(child, node);
+        if (h > max1)
+        {
+            max2 = max1;
+            max1 = h;
+        }
+        else if (h > max2)
+        {
+            max2 = h;
         }
     }
-    return depth;
+    if (max1 != -1 && max2 != -1)
+    {
+        diameter = max(diameter, max1 + max2 + 2);
+    }
+    else if (max1 != -1)
+    {
+        diameter = max(diameter, max1 + 1);
+    }
+    return max1 + 1;
 }
 
-
-int dfs1(vector<vector<int>> &tree, int node, vector<bool> &vis, int depth) {
-    vis[node] = true;
-    if (depth > max_depth1) {
-        max_depth1 = depth;
-    }
-
-    int maxi = 0;
-    for (auto i : tree[node]) {
-        if (!vis[i]) {
-            dfs(tree, i, vis, depth + 1);
-        }
-    }
-    return depth;
-}
-
-void solve(){
+void solve()
+{
     int n;
-    cin>>n;
-    vector<vector<int>> tree(n);
-    Rep(i,n-1){
-        int u,v;cin>>u>>v;
-        u--,v--;
-        tree[u].pb(v);
-        tree[v].pb(u);
+    cin >> n;
+    tree.assign(n, vector<int>());
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        tree[u].push_back(v);
+        tree[v].push_back(u);
     }
-    vector<bool> vis(n,false);
-    dfs(tree, 0, vis, 0);
-    // cout <<  max_depth << endl;
-    vector<bool> vis1(n,false);
-    dfs1(tree,realnode,vis1,0);
-    // cout << realnode << endl;
-    cout << max_depth+max_depth1 << endl;
+
+    dfs(0, -1);
+    cout << diameter << endl;
 }
-int main(){
-    int t = 1;
-    // cin>>t;
-    while(t--){
-        solve();
-    }
+
+int main()
+{
+    solve();
+    return 0;
 }
