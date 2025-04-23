0bea84912ec7f59bca2a21d4bdc0eb5f870dc7d9
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-04-23 22:24:31 +0530

    Done

---
+++ Graph/Planets_Queries_I.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> path;
+
+int main(){
+    int n,m;cin>>n>>m;
+    vector<int> parent(n);
+    for(int i=0 ; i<n ; i++)cin>>parent[i];
+    
+}
