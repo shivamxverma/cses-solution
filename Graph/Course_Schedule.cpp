fa427f3830a1db1303a1491c57a6964e2caba0f0
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-04-23 22:23:33 +0530

    Done Graph Question

--- Graph/Course_Schedule.cpp
+++ Graph/Course_Schedule.cpp
@@ -1,119 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
+vector<vector<int>> graph;
+vector<bool> vis,ok;
+bool cycle = false;
+vector<int> ans;
 
-bool dfsCycle(int node, vector<vector<int>> &adj, vector<bool> &vis, vector<bool> &recStack) {
-    vis[node] = true;
-    recStack[node] = true;
+void dfs(int u){
+    vis[u] = true;
+    ok[u] = true;
 
-    for (int neighbor : adj[node]) {
-        if (!vis[neighbor]) {
-            if (dfsCycle(neighbor, adj, vis, recStack)) return true;
-        }
-        else if (recStack[neighbor]) {
-            // Cycle detected
-            return true;
-        }
-    }
-
-    recStack[node] = false;
-    return false;
-}
-
-bool isCyclic_DFS(int V, vector<vector<int>> &adj) {
-    vector<bool> vis(V, false), recStack(V, false);
-
-    for (int i = 0; i < V; i++) {
-        if (!vis[i]) {
-            if (dfsCycle(i, adj, vis, recStack)) return true;
+    for(auto v : graph[u]){
+        if(!vis[v]){
+            dfs(v);
+            if(cycle)return;
+        } else if(ok[v]){
+            cycle = true;
+            return;
         }
     }
-
-    return false;
+    ok[u] = false;
+    ans.push_back(u);
 }
 
-// Function to perform DFS and topological sorting
-void topologicalSortUtil(int v, vector<vector<int>> &adj, vector<bool> &visited, stack<int> &st)
-{
 
-    // Mark the current node as visited
-    visited[v] = true;
+int main() {
+    cin >> n >> m;
+    graph.resize(n);
+    vis.assign(n, false);
+    ok.assign(n, false);
 
-    // Recur for all adjacent vertices
-    for (int i : adj[v])
-    {
-        if (!visited[i])
-            topologicalSortUtil(i, adj, visited, st);
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--; 
+        graph[u].push_back(v); 
     }
 
-    // Push current vertex to stack which stores the result
-    st.push(v);
-}
-
-vector<vector<int>> constructadj(int V, vector<vector<int>> &edges)
-{
-
-    vector<vector<int>> adj(V);
-    for (auto it : edges)
-    {
-        adj[it[0]].push_back(it[1]);
+    for(int i=0 ; i<n ; i++){
+        dfs(i);
+        if(cycle)break;
     }
 
-    return adj;
-}
-
-// Function to perform Topological Sort
-vector<int> topologicalSort(int V, vector<vector<int>> &edges)
-{
-
-    // Stack to store the result
-    stack<int> st;
-
-    vector<bool> visited(V, false);
-    vector<vector<int>> adj = constructadj(V, edges);
-    // Call the recursive helper function to store
-    // Topological Sort starting from all vertices one by
-    // one
-    for (int i = 0; i < V; i++)
-    {
-        if (!visited[i])
-            topologicalSortUtil(i, adj, visited, st);
+    if(!cycle){
+        reverse(ans.begin(),ans.end());
+        for(auto x : ans)cout << x << " ";
+        cout << endl;
+    } else {
+        cout << "IMPOS\n";
     }
 
-    vector<int> ans;
-
-    // Append contents of stack
-    while (!st.empty())
-    {
-        ans.push_back(st.top());
-        st.pop();
-    }
-
-    return ans;
-}
-
-int main()
-{
-    vector<vector<int>> edges;
-	int n,m;cin>>n>>m;
-	for(int i=0 ; i<m ; i++){
-		int u,v;cin>>u>>v;
-		edges.push_back({u-1,v-1});
-	}
-
-    vector<vector<int>> adj = constructadj(n, edges);
-    if (isCyclic_DFS(n, adj)) {
-        cout << "IMPOSSIBLE" << endl;
-        return 0;
-    }
-
-    vector<int> ans = topologicalSort(n, edges);
-
-    for (auto node : ans)
-    {
-        cout << node+1 << " ";
-    }
-    cout << endl;
 
     return 0;
 }
