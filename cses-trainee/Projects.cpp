9b60bef11b090375c8fb43fc900631ddbe2f847a
Shivam Verma <shivam.verma.shiva.99@gmail.com>
2025-07-29 22:55:49 +0530

    updated

--- cses-trainee/Projects.cpp
+++ cses-trainee/Projects.cpp
@@ -5,8 +5,6 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <iostream>
-#include <set>
-#include <map>
 using namespace std;
 
 // some common Defination
@@ -29,27 +27,39 @@ const int N = 1e5;
 #define rloop(i, a, b) for (int i = a; i >= b; i--)
 #define print(str) cout << str << "\n"
 
+bool cmp(vector<ll> &a,vector<ll> &b){
+    if(a[0] == b[0])return a[1]<b[1];
+    return a[0] < b[0];
+}
+
 void MahavirCoder()
 {
     int n;cin>>n;
-    vc a(n);
-    for(int i=0 ; i<n ; i++)cin>>a[i];
-    map<int,int> dp;
-    int ans = 1;
+    vector<vector<ll>> projects;
 
-    for(int i = 0 ; i<n ; i++){
-        auto it = dp.lower_bound(a[i]);
-        if(it != dp.begin()){
-            --it;
-            int count = (it->s)+1;
-            dp[a[i]] = count;
-            ans = max(ans,count);
-        } else {
-            dp[a[i]] = 1;
-        }
+    for(int i=0 ; i<n ; i++){
+        ll s,e,val;
+        cin>>s>>e>>val;
+        projects.push_back({s,e,val});
     }
 
-    cout << ans << endl;
+    sort(projects.begin(), projects.end(), [](const vector<ll>& a, const vector<ll>& b) {
+        return a[1] < b[1];
+    });
+
+    vector<ll> dp(n, 0);
+
+    vector<ll> ends(n);
+    for (int i = 0; i < n; ++i) ends[i] = projects[i][1];
+
+    for (int i = 0; i < n; ++i) {
+        ll incl = projects[i][2];
+        int l = lower_bound(ends.begin(), ends.end(), projects[i][0]) - ends.begin() - 1;
+        if (l >= 0) incl += dp[l];
+        dp[i] = max((i > 0 ? dp[i-1] : 0), incl);
+    }
+
+    cout << dp[n-1] << endl;
 }
 
 int main()
